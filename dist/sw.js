fa1e53c48d624f603973d9f3873039fbc2e8c5e0
growbro <growbrohk@gmail.com>
2026-04-17 23:12:37 +0800

    update pin

--- dist/sw.js
+++ dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3d80fe8882d96b79b78989faee062706"
   }, {
     "url": "index.html",
-    "revision": "a2d77ff8023c5a33252568397bd75c48"
+    "revision": "9d8c0162d2a3a5c40b6aabe8866a34b9"
   }, {
     "url": "favicon.png",
     "revision": "bded9006240c02849570f21f32689383"
@@ -195,7 +195,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "url": "brand/app-mark.png",
     "revision": "9653e59371e12a7e2187c0a935d0d19c"
   }, {
-    "url": "assets/index-BuUgf9JK.js",
+    "url": "assets/index-CaEdhkS5.js",
     "revision": null
   }, {
     "url": "assets/index-BVYe3rau.css",
